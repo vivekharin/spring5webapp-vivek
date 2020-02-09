41ecd57fbe212cc0ad629665723a18ab615b2472
Vivek Hari Narayanan <vivek.hari.n@gmail.com>
2020-02-10 07:20:15 +1100

    Book Controller fixed and Author Controller introduced

--- src/main/java/guru/springframework/spring5webapp/controller/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -17,6 +17,6 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model) {
     model.addAttribute("books",bookRepository.findAll());
-        return "list";
+        return "books/list";
     }
 }
